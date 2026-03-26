b67f6a77a110a8989a9bc6e4afccc0616eaca278
nataliadyszer <natuska2909@gmail.com>
2026-03-26 17:03:18 +0100

    lab03

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,28 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      home: Text("hello"),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("KrakFlow"),
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text("KrakFlow"),
+              Text("Organizacja studiów"),
+              Text("Dzisiejsze zadania"),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
