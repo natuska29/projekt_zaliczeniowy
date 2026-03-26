f44d4f94f15358cbb3596d7a9109629efb938baf
nataliadyszer <natuska2909@gmail.com>
2026-03-26 17:44:59 +0100

    lab04

--- lib/main.dart
+++ lib/main.dart
@@ -5,23 +5,107 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+
+  final List<Task> tasks = [
+    Task(title: "nauka do kolosa", deadline: "dzisiaj", done: false, priority: "wysoki"),
+    Task(title: "kupic bilety na pociąg", deadline: "do końca tygodnia", done: true, priority: "średni"),
+    Task(title: "zrobic zakupy", deadline: "jutro", done: false, priority: "niski"),
+    Task(title: "oplacic rachunki", deadline: "do końca miesiąca", done: true, priority: "wysoki"),
+  ];
+
   @override
   Widget build(BuildContext context) {
+    int doneCount = tasks.where((t) => t.done).length;
+
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text("KrakFlow"),
         ),
-        body: Center(
+        body: Padding(
+          padding: EdgeInsets.all(16),
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text("KrakFlow"),
-              Text("Organizacja studiów"),
-              Text("Dzisiejsze zadania"),
+
+              Text("Masz dziś ${tasks.length} zadania (zrobione: $doneCount)"),
+              SizedBox(height: 16),
+
+              // ✅ Nagłówek
+              Text(
+                "Dzisiejsze zadania",
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+
+              SizedBox(height: 16),
+
+              Expanded(
+                child: ListView.builder(
+                  itemCount: tasks.length,
+                  itemBuilder: (context, index) {
+                    final task = tasks[index];
+
+                    return TaskCard(
+                      title: task.title,
+                      subtitle: "termin: ${task.deadline} | priorytet: ${task.priority}",
+                      icon: task.done
+                          ? Icons.check_circle
+                          : Icons.radio_button_unchecked,
+                    );
+                  },
+                ),
+              ),
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+//////////////////////////////////////////////////
+// MODEL (zadanie 1 + 6)
+
+class Task {
+  final String title;
+  final String deadline;
+  final bool done;
+  final String priority;
+
+  Task({
+    required this.title,
+    required this.deadline,
+    required this.done,
+    required this.priority,
+  });
+}
+
+class TaskCard extends StatelessWidget {
+  final String title;
+  final String subtitle;
+  final IconData icon;
+
+  const TaskCard({
+    super.key,
+    required this.title,
+    required this.subtitle,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: EdgeInsets.only(bottom: 12),
+      child: ListTile(
+        leading: Icon(icon, color: Colors.blue),
+        title: Text(
+          title,
+          style: TextStyle(fontWeight: FontWeight.bold),
+        ),
+        subtitle: Text(subtitle),
       ),
     );
   }
